ee1f97336292a9a88ce66a47e4024acc82b24eda
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-07-05 18:37:24 +0200

    fixed some bug

--- js/dataTablesBB.js
+++ js/dataTablesBB.js
@@ -55,19 +55,19 @@
         },
         comparator: function (item) {
             if(!isNaN(parseFloat(item.get(this.sort_key))) && isFinite(item.get(this.sort_key))){
-                if(this.sort_order === 'asc'){
+                if(this.sort_order == 'asc'){
                     return parseFloat(item.get(this.sort_key));
                 }
                 else{
                     return -parseFloat(item.get(this.sort_key));
                 }
             }
-            if(typeof item.get(this.sort_key) === 'string') {
+            if(typeof item.get(this.sort_key) == 'string') {
                 var check = item.get(this.sort_key).split('/');
                 var check2 = item.get(this.sort_key).split('-');
-                if((parseInt(check.length) === '3') || (parseInt(check2.length) === '3')){
+                if((check.length == '3') || (check2.length == '3')){
                     var date = new Date(item.get(this.sort_key));
-                    if(this.sort_order === 'asc'){
+                    if(this.sort_order == 'asc'){
                         return date.getTime();
                     }
                     else{
@@ -75,7 +75,7 @@
                     }
                 }
                 else{
-                    if(this.sort_order === 'desc'){
+                    if(this.sort_order == 'desc'){
                         var alphabet = '<>-_0123456789abcdefghijklmnopqrstuvwxyz',
                             title = item.get(this.sort_key).toString().toLowerCase(),
                             inverse_title = '',
@@ -84,7 +84,7 @@
                         for(var i=0, len=title.length; i<len; i+=1) {
                             index = alphabet.indexOf(title.charAt(i));
                          
-                            if(parseInt(index) === -1) {
+                            if(index == -1) {
                                 inverse_title += title.charAt(i);
                                 continue;
                             }
@@ -174,6 +174,7 @@
                     }
                 })
             ).then(function() {
+                here.data_table.sortByField(here.order, here.order_by);
                 here.render();
             });
 
@@ -194,7 +195,7 @@
 
                 $(document).find(here.el).find('.order-arrow').remove();
                 $(document).find(here.el).find('tr').each(function(index){
-                    if(parseInt(index) === 1){
+                    if(index == 1){
                         if($(this).find('.order-by').length <= 0){
                             $(this).find('td').each(function(index2){
                                 if($(this).hasClass('to-order')){
@@ -208,16 +209,14 @@
                                         margin = 0;
                                     }
 
-                                    if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'asc')){
+                                    if((here.order == index2) && (here.order_by == 'asc')){
                                         asc_img = 'img/sort_asc.png';
                                         opacity_desc = 0;
-                                        console.log('g');
                                     }
 
-                                    if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'desc')){
+                                    if((here.order == index2) && (here.order_by == 'desc')){
                                         desc_img = 'img/sort_desc.png';
                                         opacity_asc = 0;
-                                        console.log('h');
                                     }
 
                                     $(this).find('.container-arrow').append('<div style="position:absolute; top:' + margin + 'px; right: 6px; width:10px; height:10px;" class="order-arrow"><img src="' + asc_img + '" alt="" data-order="asc-' + index2 + '" class="order-by" style="opacity:' + opacity_asc + '"/><img src="' + desc_img + '" alt="" data-order="desc-' + index2 + '" class="order-by" style="margin-top: -25px; opacity:' + opacity_desc + '"/></div>');
@@ -236,12 +235,12 @@
                                         margin = 0;
                                     }
 
-                                    if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'asc')){
+                                    if((here.order == index2) && (here.order_by == 'asc')){
                                         asc_img = 'img/sort_asc.png';
                                         opacity_desc = 0;
                                     }
 
-                                    if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'desc')){
+                                    if((here.order == index2) && (here.order_by == 'desc')){
                                         desc_img = 'img/sort_desc.png';
                                         opacity_asc = 0;
                                     }
@@ -266,12 +265,12 @@
                         var to_filter = 0;
                         _.each(data.attributes, function(d, index_d) {
                             _.each(here.filterColumn, function(filter) {
-                                if (parseInt(filter) === parseInt(index_d)){
+                                if (filter == index_d){
                                     to_filter = 1;
                                 }
                             });
 
-                            if(parseInt(to_filter) === 1){
+                            if(to_filter == 1){
                                 if((index_t !== 'id') && (index_t !== 'show')){
                                     var text = d.toString().toLowerCase();
                                     
@@ -289,7 +288,7 @@
 
                             if(text.indexOf(searched_text.toLowerCase()) >= 0){
                                 _.each(here.data_table.models, function(data2) {
-                                    if(parseInt(data2.attributes.id) === parseInt(data.attributes.data_id)){
+                                    if(data2.attributes.id == data.attributes.data_id){
                                         data2.set('show', '1');
                                     }
                                 });
@@ -305,7 +304,7 @@
             $(here.el).parent().on('click', '#table-request_paginate .paginate_button', function(){
                 if($(this).hasClass('previous')){
                     if(here.actual_page > 1){
-                        var new_page = parseInt(here.actual_page) - 1;
+                        var new_page = here.actual_page - 1;
                     
                         here.actual_page = new_page;
                         here.render();
@@ -314,7 +313,7 @@
                 else{
                     if($(this).hasClass('next')){
                         if(here.actual_page < here.max_page){
-                            var new_page_change = parseInt(here.actual_page) + 1;
+                            var new_page_change = here.actual_page + 1;
                         
                             here.actual_page = new_page_change;
                             here.render();
@@ -369,7 +368,7 @@
             $(here.el).on('click', '.remove-el', function(){
                 var id = $(this).closest('tr').attr('id').substr(4);
                 _.each(here.data_table.models, function(data) {
-                    if(parseInt(data.attributes.id) === parseInt(id)){
+                    if(data.attributes.id == id){
                         here.data_table.removeConfiguration(data);
                         here.emptyPagination();
                         here.render();
@@ -391,7 +390,7 @@
             var here = this;
             var option_max = '';
             for (var j = 5; j<=100; j+=5){
-                if(parseInt(j) === parseInt(here.max)){
+                if(j == here.max){
                     option_max += '<option value="' + j + '" selected="selected">' + j + '</option>';
                 }
                 else{
@@ -441,12 +440,12 @@
                                     margin = 0;
                                 }
 
-                                if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'asc')){
+                                if((here.order == index2) && (here.order_by == 'asc')){
                                     asc_img = 'img/sort_asc.png';
                                     opacity_desc = 0;
                                 }
 
-                                if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'desc')){
+                                if((here.order == index2) && (here.order_by == 'desc')){
                                     desc_img = 'img/sort_desc.png';
                                     opacity_asc = 0;
                                 }
@@ -467,18 +466,17 @@
                                 var asc_img = 'img/sort_asc_disabled.png';
                                 var desc_img = 'img/sort_desc_disabled.png';
 
-                                console.log(here.order_by + ' - ' + here.order + ' - ' + index2);
 
                                 if(($(this).html().indexOf('<br />') >= 0) || ($(this).html().indexOf('<br>') >= 0) || ($(this).html().indexOf('<br/>') >= 0)){
                                     margin = 0;
                                 }
 
-                                if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'asc')){
+                                if((here.order == index2) && (here.order_by == 'asc')){
                                     asc_img = 'img/sort_asc.png';
                                     opacity_desc = 0;
                                 }
 
-                                if((parseInt(here.order) === parseInt(index2)) && (here.order_by === 'desc')){
+                                if((here.order == index2) && (here.order_by == 'desc')){
                                     desc_img = 'img/sort_desc.png';
                                     opacity_asc = 0;
                                 }
@@ -505,21 +503,21 @@
 
             $.when(
                 _.each(here.data_table.models, function(t) {
-                    if(t.attributes.show === '1'){
+                    if(t.attributes.show == '1'){
                         _.each(here.data_tip.models, function(t2) {
-                            if(parseInt(t2.attributes.data_id) === parseInt(t.attributes.id)){
+                            if(t2.attributes.data_id == t.attributes.id){
                                 data_tip_send = t2;
                             }
                         });
 
                         _.each(here.style_td.models, function(t2) {
-                            if(parseInt(t2.attributes.data_id) === parseInt(t.attributes.id)){
+                            if(t2.attributes.data_id == t.attributes.id){
                                 style_send = t2;
                             }
                         });
 
                         _.each(here.class_table.models, function(t2) {
-                            if(parseInt(t2.attributes.data_id) === parseInt(t.attributes.id)){
+                            if(t2.attributes.data_id == t.attributes.id){
                                 class_send = t2;
                             }
                         });
@@ -561,7 +559,7 @@
                         var number_pages = Math.ceil(parseInt(count_elements) / parseInt(here.max));
                         here.max_page = number_pages;
                         for (var i = number_pages; i > 0; i-=1){
-                            if(parseInt(here.actual_page) === i){
+                            if(here.actual_page == i){
                                 $(here.el).parent().find('.table-request_previous').after('<span><a class="paginate_button current" aria-controls="table-request" data-dt-idx="' + i + '" tabindex="0">' + i + '</a></span>');
                             }
                             else{
@@ -573,7 +571,7 @@
                         $(here.el).parent().find('.paginate_button').each(function(index){
                             $(this).removeClass('current');
 
-                            if(parseInt(here.actual_page) === index){
+                            if(here.actual_page == index){
                                 $(this).addClass('current');
                             }
                         });
